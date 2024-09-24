0a0622960d0cb594cbc54a075b955e800c744431
Alexander Bangert <alexganbert@gmail.com>
2024-09-24 13:07:39 +0500

    local storage, update task, ui

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,23 @@
-window.onload = (event) => {
-    let tables = [
-        {
-            title: 'Todo',
-            tasks: [
-                {
-                    id: 1,
-                    title: 'Task 1',
+const initialTables = [
+    {
+        title: 'Todo',
+        tasks: []
+    },
+    {
+        title: 'In Progress',
+        tasks: []
+    },
+    {
+        title: 'Done',
+        tasks: []
+    }
+];
 
-                }
-            ]
-        },
-        {
-            title: 'In Progress',
-            tasks: []
-        },
-        {
-            title: 'Done',
-            tasks: []
-        }
-    ]
+
+window.onload = (event) => {
+    let tables = initialTables;
+
+    let updatedTaskId = null;
 
     const nextTaskAction = (task) => (event) => {
         const taskId = task.id;
@@ -51,6 +50,44 @@ window.onload = (event) => {
         return tables;
     }
 
+    const formAction =  (event) => {
+        event.preventDefault();
+        if(updatedTaskId) {
+            putTaskAction(event);
+        } else {
+            createTaskAction(event);
+        }
+        render();
+    }
+
+    const putTaskAction = (event) => {
+        const formData = new FormData(event.target);
+        const title = formData.get('title');
+        tables = tables.map(table => {
+            table.tasks = table.tasks.map(task => {
+                if(task.id === updatedTaskId) {
+                    task.title = title;
+                }
+                return task;
+            });
+            return table;
+        });
+        updatedTaskId = null;
+    }
+
+    const createTaskAction = (event) => {
+        const formData = new FormData(event.target);
+
+        const title = formData.get('title');
+        const id = Date.now();
+        tables[0].tasks.push({ id, title });
+    }
+
+    const updateTaskAction = (task) => (event) => {
+        updatedTaskId = task.id;
+        render();
+    }
+
     const createTask = (task) => {
         const { title, id } = task;
         
@@ -58,23 +95,37 @@ window.onload = (event) => {
         taskElement.classList.add('card', 'mb-1');
         taskElement.id = `task.${id}`;
 
-        let buttonElement = document.createElement('button');
-        buttonElement.classList.add('btn', 'btn-primary');
-        buttonElement.appendChild(document.createTextNode('Next'));
-        buttonElement.addEventListener('click', nextTaskAction(task));
+        let taskBodyElement = document.createElement('div');
+
+        let titleElement = document.createElement('p');
+        titleElement.appendChild(document.createTextNode(title));
+        taskBodyElement.appendChild(titleElement);
+
+
+        
+        nextTitleTable = tables[tables.findIndex(table => table.tasks.find(task => task.id === id)) + 1]?.title ?? null;
+        if(nextTitleTable) {
+            let buttonElement = document.createElement('button');
+            buttonElement.classList.add('btn', 'btn-primary');
+            buttonElement.appendChild(document.createTextNode(nextTitleTable));
+            buttonElement.addEventListener('click', nextTaskAction(task));
+            taskBodyElement.appendChild(buttonElement);
+        }
 
         let deleteButtonElement = document.createElement('button');
         deleteButtonElement.classList.add('btn', 'btn-danger', 'ml-2');
         deleteButtonElement.appendChild(document.createTextNode('Delete'));
         deleteButtonElement.addEventListener('click', deleteTaskAction(task));
 
-        let titleElement = document.createElement('p');
-        titleElement.appendChild(document.createTextNode(title));
-
-        let taskBodyElement = document.createElement('div');
+        if(nextTitleTable) {
+            let updateButtonElement = document.createElement('button');
+            updateButtonElement.classList.add('btn', 'btn-warning', 'ml-2');
+            updateButtonElement.appendChild(document.createTextNode('Update'));
+            updateButtonElement.addEventListener('click', updateTaskAction(task));
+            taskBodyElement.appendChild(updateButtonElement);
+        }
+        
         taskBodyElement.classList.add('card-body');
-        taskBodyElement.appendChild(titleElement);
-        taskBodyElement.appendChild(buttonElement);
         taskBodyElement.appendChild(deleteButtonElement);
 
         taskElement.appendChild(taskBodyElement);
@@ -121,21 +172,12 @@ window.onload = (event) => {
         return listTablesElement;
     }
 
-    const createTaskAction =  (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-
-        const title = formData.get('title');
-        const id = Date.now();
-        tables[0].tasks.push({ id, title });
-        render();
-    }
-
     const createForm = () => {
         let formElement = document.getElementById('form');
         formElement.innerHTML = '';
-        formElement.removeEventListener('submit', createTaskAction)
-        formElement.addEventListener('submit', createTaskAction)
+
+        formElement.removeEventListener('submit', formAction)
+        formElement.addEventListener('submit', formAction)
 
         let inputWrappedElement = document.createElement('div');
         inputWrappedElement.classList.add('col-8');
@@ -145,10 +187,13 @@ window.onload = (event) => {
         inputElement.setAttribute('placeholder', 'Name New Todo');
         inputElement.setAttribute('name', 'title');
 
+        task = getTaskById(updatedTaskId);
+        inputElement.setAttribute('value', task ? task.title : '');
+
         let buttonElement = document.createElement('input');
         buttonElement.classList.add('btn', 'btn-primary', 'col-4');
         buttonElement.setAttribute('type', 'submit');
-        buttonElement.setAttribute('value', 'Create');
+        buttonElement.setAttribute('value', task? 'Update' : 'Create');
         
 
         inputWrappedElement.appendChild(inputElement);
@@ -157,11 +202,41 @@ window.onload = (event) => {
 
         return formElement;
     }
+
+    const getTaskById = (id) => {
+        return tables.reduce((acc, table) => {
+            const task = table.tasks.find(task => task.id === id);
+            if(task) {
+                acc = task;
+            }
+            return acc;
+        }, null);
+    }
     
     const render = () => {
+        setStorage(tables);
         createForm();
         createTables(tables);
+
+        return true;
     };
 
-    render();
+    const setStorage = (tables) => {
+        const tablesJSON = JSON.stringify(tables);
+        localStorage.setItem('tables', tablesJSON);
+        
+        return tables;
+    }
+
+    const getStorage = () => {
+        tables = JSON.parse(localStorage.getItem('tables')) ?? initialTables;
+        return tables;
+    }
+
+    const init = () => {
+        getStorage();
+        render();
+    }
+
+    init();
 };
